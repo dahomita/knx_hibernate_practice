840851f00a47ae6fe635ec288e63694b72550748
Daho Mita <dahomita17062006@gmail.com>
2025-04-09 17:51:22 -0400

    Initial commit: Spring Boot Maven Project

---
+++ src/test/java/com/example/demo/CrudRestJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
